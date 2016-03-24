b4fe4cbe0525cb413d74d67f8c1e1ff53ba1c476
Marcel Caroly <mcaroly@gmail.com>
2016-03-24 20:18:22 +1100

    Removing unused logger.

--- src/main/java/info/mcaroly/vaadin/app/Application.java
+++ src/main/java/info/mcaroly/vaadin/app/Application.java
@@ -1,7 +1,5 @@
 package info.mcaroly.vaadin.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,8 +10,6 @@ import java.io.IOException;
  */
 @SpringBootApplication
 public class Application {
-
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class);
